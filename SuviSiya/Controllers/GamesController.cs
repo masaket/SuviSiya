a57f45537f9b1d31a79043f352f4b6168a26878a
Saket Adhikari <saket.adhikari@RealPage.com>
2019-06-03 14:25:11 -0500

    update

---
+++ SuviSiya/Controllers/GamesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SuviSiya.Models;
+
+namespace SuviSiya.Controllers
+{
+    public class GamesController : Controller
+    {
+
+        // GET: Games/Random
+        public ActionResult Random()
+        {
+            var games = new Games() { Name = "Snake!" };
+            return View(games);
+        }
+    }
+}
---
+++ SuviSiya/Models/SuviSiya.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SuviSiya.Models;
+
+namespace SuviSiya.Models
+{
+    public class Games
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
